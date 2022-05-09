8b41186ea7e798ac823649c80180b07c78b21d0b
Kaivalyamani Tripathi <60978175+Kaivalya4@users.noreply.github.com>
2022-05-09 12:45:29 +0530

    Time: 26 ms (51.97%), Space: 11.6 MB (66.01%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
     vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
-        vector<int> q;
+        vector<int> fcount(11,0);
+        for(int i=0;i<words.size();i++){
+            sort(words[i].begin(),words[i].end());
+            int ans = -1;
+            for(int j=0;j<words[i].size()-1;j++){
+                if(words[i][j] != words[i][j+1]){
+                    ans = j;
+                    break;
+                }  
+            }
+            if(ans == -1){
+                ans = words[i].length();
+            }
+            else{
+                ans++;
+            }
+            fcount[ans]++;
+        }
+        for(int i=1;i<fcount.size();i++)
+            fcount[i] += fcount[i-1];
+        
+        vector<int> ans1;
         for(int i=0;i<queries.size();i++){
             sort(queries[i].begin(),queries[i].end());
             
@@ -18,38 +39,10 @@ public:
             else{
                 ans++;
             }
-            
-            q.push_back(ans);
+            ans1.push_back(fcount[10] - fcount[ans]);
         }
-        vector<int> w;
-        for(int i=0;i<words.size();i++){
-            sort(words[i].begin(),words[i].end());
-            int ans = -1;
-            for(int j=0;j<words[i].size()-1;j++){
-                if(words[i][j] != words[i][j+1]){
-                    ans = j;
-                    break;
-                }  
-            }
-            if(ans == -1){
-                ans = words[i].length();
-            }
-            else{
-                ans++;
-            }
-            w.push_back(ans);
-        }
-        vector<int> ans;
-        sort(w.rbegin(),w.rend());
-        for(int i=0;i<q.size();i++){
-            int  j=0;
-            while(j<w.size()){
-                if(q[i]>=w[j])
-                    break;
-                j++;
-            }
-                ans.push_back(j);
-        }
-        return ans;
+        
+        
+        return ans1;
     }
 };
