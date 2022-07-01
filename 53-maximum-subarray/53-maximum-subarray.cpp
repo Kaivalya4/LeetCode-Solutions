fc661adebb8e06a6ce1fbd862519050b00bf3697
Kaivalyamani Tripathi <60978175+Kaivalya4@users.noreply.github.com>
2022-07-01 13:27:06 +0530

    Time: 209 ms (17.85%), Space: 78 MB (5.88%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int sum = 0;
-        int maxi = nums[0];
-        for(auto i:nums){
-            sum += i;
-            maxi = max(sum , maxi);
-            if(sum<0)
-                sum = 0;
+    int dp[100001][2];
+    int solve(int idx , bool picked , vector<int> &nums){
+        if(idx == nums.size())
+        {
+            if(picked)
+                return 0;
+            return -10000001;
         }
-        return maxi;
+        if(dp[idx][picked] != -1)
+            return dp[idx][picked];
+        if(picked)
+            return dp[idx][picked] = max(0,nums[idx] +solve(idx+1,true,nums));
+        return dp[idx][picked] = max(solve(idx +1, false , nums) , nums[idx] + solve(idx +1, true , nums));
+    }
+    int maxSubArray(vector<int>& nums) {
+        memset(dp , -1 , sizeof(dp));
+        
+        return solve(0 , false , nums);
     }
 };
