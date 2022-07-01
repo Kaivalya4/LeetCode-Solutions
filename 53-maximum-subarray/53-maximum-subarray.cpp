a6386134045a4347047e37b25170bd546b6230b3
Kaivalyamani Tripathi <60978175+Kaivalya4@users.noreply.github.com>
2022-07-01 14:09:19 +0530

    Time: 236 ms (7.57%), Space: 68.2 MB (6.71%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    int dp[100001][2];
-    int solve(int idx , bool picked , vector<int> &nums){
-        if(idx == nums.size())
-        {
-            if(picked)
-                return 0;
-            return -10000001;
-        }
-        if(dp[idx][picked] != -1)
-            return dp[idx][picked];
-        if(picked)
-            return dp[idx][picked] = max(0,nums[idx] +solve(idx+1,true,nums));
-        return dp[idx][picked] = max(solve(idx +1, false , nums) , nums[idx] + solve(idx +1, true , nums));
-    }
+    int dp[100001];
     int maxSubArray(vector<int>& nums) {
-        memset(dp , -1 , sizeof(dp));
-        dp[0][0] = -10000001; 
-        dp[0][1] = 0;
+        dp[0] = 0;
+        int maxi  = nums[0];
         int n = nums.size();
         for(int i=1;i<=n;i++){
-            dp[i][1] = max(0 , nums[i-1] + dp[i-1][1]);
-            dp[i][0] = max(dp[i-1][0] , nums[i-1] + dp[i-1][1]);
+            dp[i] = max(nums[i-1] , nums[i-1] + dp[i-1]);
+            maxi = max(maxi , dp[i]);
         }
         
-        return dp[n][0];
+        return maxi;
     }
 };
