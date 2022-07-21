869ffd3dac274e5fd761aa70ada21365e02528dc
Kaivalyamani Tripathi <60978175+Kaivalya4@users.noreply.github.com>
2022-07-21 12:37:59 +0530

    Time: 1201 ms (5.00%), Space: 120.1 MB (5.00%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     
-    set<vector<int>> s;
     map<string,int> umap;
     
-    int solve(int idx , string mark , vector<int>&c , int t , vector<int>&sol){
+    int solve(int idx , string mark , vector<int>&c , int t , vector<int>&sol , vector<vector<int>> &ans){
+        if(umap[mark])
+            return umap[mark] ;
         if(t == 0){
-             umap[mark] = 1;
-            s.insert(sol);
+            umap[mark] = 1;
+            ans.push_back(sol);
             return 1;
         }
         if(idx == c.size()){
-            return 2;
+            return umap[mark]=2;
         }
         if(t<0)
-            return 2;
-        if(umap[mark])
-            return umap[mark] ;
+            return umap[mark]=2;
+        
         sol.push_back(c[idx]);
         mark[c[idx]] ++;
-        int ans1 = solve(idx+1,mark,c,t-c[idx],sol);
+        int ans1 = solve(idx+1,mark,c,t-c[idx],sol , ans);
         sol.pop_back();
         mark[c[idx]] --;
-        int ans2 = solve(idx+1,mark,c,t,sol);
+        int ans2 = solve(idx+1,mark,c,t,sol , ans);
         return umap[mark] = (ans1 == 1 || ans2 == 1 ? 1 : 2);
     }
     
@@ -31,10 +31,7 @@ public:
         vector<vector<int>> ans;
         vector<int> sol; 
         string mark(51,'0');
-        solve(0,mark,candidates,target , sol);
-        for(auto t:s){
-            ans.push_back(t);
-        }
+        solve(0,mark,candidates,target , sol , ans);
         return ans;
     }
 };
