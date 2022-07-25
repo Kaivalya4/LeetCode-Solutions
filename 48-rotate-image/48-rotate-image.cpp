06c981a56db3f42f44c37db7b5bbd198fe0e5ab0
Kaivalyamani Tripathi <60978175+Kaivalya4@users.noreply.github.com>
2022-07-25 17:44:57 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (33.20%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& a) {
-        int N = a.size();
-        ///for every cycle
-       for (int i = 0; i < N / 2; i++) {
-        for (int j = i; j < N - i - 1; j++) { //from(i,i) to (i,n-1-i) i.e top right corner of each cycle
- 
-            // Swap elements of each cycle
-            // in clockwise direction
-            int temp = a[i][j];  ///top left 
-            a[i][j] = a[N - 1 - j][i]; //bottom left 
-            a[N - 1 - j][i] = a[N - 1 - i][N - 1 - j]; //bottom right
-            a[N - 1 - i][N - 1 - j] = a[j][N - 1 - i]; //top right
-            a[j][N - 1 - i] = temp;
+        int n = a.size();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                swap(a[i][j] , a[j][i]);
+            }
         }
-    }
+        for(int i=0;i<n;i++){
+            reverse(a[i].begin() , a[i].end());
+        }
     }
 };
