ce8287597d40f670e3ea0b0a385f666a68bf4680
Kaivalyamani Tripathi <60978175+Kaivalya4@users.noreply.github.com>
2022-07-26 12:31:35 +0530

    Time: 19 ms (62.89%), Space: 15.9 MB (51.28%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> vec;
-        sort(nums.begin() , nums.end());
+        int candidate1 = INT_MAX, candidate2  = INT_MAX;
+        int count1 = 0 , count2 =0;
         int n = nums.size();
-        int ctr =0;
-        for(int i=0;i<n;){
-            int val = nums[i];
-            ctr = 0;
-            while(i<n && nums[i] == val ){
-                ctr++;
-                i++;
+        vector<int> sol;
+        for(auto i:nums){
+            if(candidate1 == i){
+                count1++;
+            } else if(candidate2 == i){
+                count2++;
+            } else if( count1 ==0){
+                candidate1 = i;
+                count1++;
+            } else if(count2 == 0){
+                candidate2 =i ;
+                count2++;
+            } else{
+                count1-- ,count2--;
             }
-            if(ctr > n/3)
-                vec.push_back(val);
+            
         }
-        return vec;
+        count1 =0 , count2 =0;
+        for(auto i:nums){
+            if(i == candidate1)
+                count1++;
+            if(i == candidate2)
+                count2++;
+        }
+        if(count1 > n/3)
+            sol.push_back(candidate1);
+        if(count2 > n/3 )
+            sol.push_back(candidate2);
+        return sol;
     }
 };
